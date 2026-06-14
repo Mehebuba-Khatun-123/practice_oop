825f4f508df7c29c454ca08f08b37ec8bb70f0ae
Mehebuba-Khatun-123 <mehebubak649@gmail.com>
2026-06-14 21:35:38 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+class User{
+    public $name;
+    public $roll;
+    public $email;
+    public function introduce(){
+        echo "Hi, i am {$this->name}. My roll is {$this->roll} and my email is {$this->email}.";
+    }
+}
+$user = new user();
+$user->name ="Mehebuba";
+$user->roll =10;
+$user->email ="mehebuba@gmail.com";
+$user->introduce();
+?>
